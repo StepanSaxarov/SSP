7e64263696caa28850f297ad277cf52566012784
StepanSaxarov <stepansaxarov@yandex.ru>
2023-05-31 16:16:53 +0300

    смена на long

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = new int[]{1, 10, 100, 1000};
+        long[] sales = new long[]{1, 10, 100, 1000};
         SalesManager salesmanager = new SalesManager(sales);
-        int max = salesmanager.max();
+        long max = salesmanager.max();
         System.out.println(max);
     }
+
+
 }
