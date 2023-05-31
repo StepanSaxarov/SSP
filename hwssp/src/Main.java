3c9d0cd32e7d49b4f50ab3beccd9cfdbcfef570d
StepanSaxarov <stepansaxarov@yandex.ru>
2023-05-31 13:27:13 +0300

    readme

--- hwssp/src/Main.java
+++ hwssp/src/Main.java
@@ -1,4 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        int[] sales = new int[]{1, 10, 100, 1000};
+        SalesManager salesmanager = new SalesManager(sales);
+        int max = salesmanager.max();
+        System.out.println(max);
+    }
 
 
 }
