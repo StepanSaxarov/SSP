edf33dc546c79477827606c64d1676354a89cef2
StepanSaxarov <stepansaxarov@yandex.ru>
2023-05-31 16:09:22 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sales = new int[]{1, 10, 100, 1000};
+        SalesManager salesmanager = new SalesManager(sales);
+        int max = salesmanager.max();
+        System.out.println(max);
+    }
+}
